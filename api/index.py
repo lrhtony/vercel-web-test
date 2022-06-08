f5f4d3ebd53081dfdc684452528513d57600ccb8
lrhtony <tony07017@163.com>
2022-06-08 23:46:39 +0800

    test sanic test

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from sanic import Sanic
-from sanic.response import redirect
+from sanic.response import redirect, text
 
 app = Sanic('vercel-sanic-test')
 
@@ -7,6 +7,11 @@ app = Sanic('vercel-sanic-test')
 @app.route('/')
 async def index(request):
     return redirect('https://vercel.com/')
+
+
+@app.route('/ping')
+async def pong(request):
+    return text('pong!')
 
 if __name__ == '__main__':
     app.run(auto_reload=True, port=8000)
